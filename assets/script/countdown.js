dec76fef81bcc596db0486fae039462646b2c01b
Thorgathis <92544658+Thorgathis@users.noreply.github.com>
2024-12-13 19:11:37 +0300

    Update countdown.js

--- assets/script/countdown.js
+++ assets/script/countdown.js
@@ -54,7 +54,7 @@ const countdown = function (_config) {
             (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
         );
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000) + 1;
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         requestAnimationFrame(updateTime);
 
